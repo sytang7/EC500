f143c355736a758d867af6baa55a1d0e0d4e23ef
Zatl <tsy.cpp@gmail.com>
2018-02-05 17:31:19 -0500

    update

--- Assignment1/tweettest.py
+++ Assignment1/tweettest.py
@@ -7,84 +7,80 @@ import io
 from google.cloud import vision
 from google.cloud.vision import types
 from google.cloud import bigquery
-import ffmpy
 import os
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
 
-def get_all_tweets_image(screen_name):
-    
-    #Twitter only allows access to a users most recent 3240 tweets with this method
-    
-    #authorize twitter, initialize tweepy
+
+def get_all_tweets_imageurls(screen_name = "@NintendoAmerica", Num = 100):    
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
+
+
+
     alltweets = []    
     new_tweets = api.user_timeline(screen_name = screen_name,count=10)
     alltweets.extend(new_tweets)  
     oldest = alltweets[-1].id - 1
-    res = []
-    filename = []
+    imageurls = []
+    filenames = []
     while len(new_tweets) > 0:
-        print(len(alltweets))
-        #all subsiquent requests use the max_id param to prevent duplicates
         new_tweets = api.user_timeline(screen_name = screen_name,count=10,max_id=oldest)
         for status in new_tweets:
             if 'media' in status._json['entities']:
-                res.append(status._json['entities']['media'][0]['media_url'])
-                filename.append(status._json['id_str'])
-        #save most recent tweets
+                imageurls.append(status._json['entities']['media'][0]['media_url'])
+                filenames.append(status._json['id_str'])
         alltweets.extend(new_tweets)
         
-        #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1
-        if(len(alltweets) > 100):
+        if(len(alltweets) > Num):
+            print("Got {} images from {} so far".format(len(imageurls),screen_name))
             break
-        print("...%d tweets downloaded so far".format(len(alltweets)))
-    return res, filename
+        
+    return imageurls, filenames
  
-def download_image(imageurl,filename):
-    for url,name in zip(imageurl,filename):
+def download_image(imageurls,filenames):
+    if os.path.exists("photo/"):
+        os.system('rm -rf photo/*')
+    else:
+        os.system('mkdir photo')
+    for url,name in zip(imageurls, filenames):
         directory = "photo/" + name + '.jpg'
         urllib.request.urlretrieve(url, directory)
+    print("Finish Downloading!")
+    print("------------------------------------")
 
 
-def vision_analysor(imageurl):
+def vision_analysor(imageurls,filenames):
     client = vision.ImageAnnotatorClient()
     image = types.Image()
-    image.source.image_uri = imageurl
-    response = client.face_detection(image=image)
-    faces = response.face_annotations
+    file = open('results.txt', 'w')
+    for url,name in zip(imageurls,filenames):
+        image.source.image_uri = url
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+        file.write('Labels for {}:\n'.format(name))
+        for label in labels:
+            file.write(label.description + "\n")
+        file.write('-----------------------------------------\n')
 
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
-    print('Labels:')
-
-    for label in labels:
-        print(label.description)
-
-def convert_image_video(filename):
+def convert_image_video(filenames, outputname = 'output.mp4'):
     file = open('photo/filenames.txt', 'w')
-    for name in filename:
+    for name in filenames:
         inputfile = "photo/" + name + '.jpg'
         outputfile = "photo/" + name + '.mp4'
         cmd = "ffmpeg -loop 1 -i " + inputfile + " -c:a libfdk_aac -ar 44100 -ac 2 -vf \"scale='if(gt(a,16/9),1280,-1)\':\'if(gt(a,16/9),-1,720)\', pad=1280:720:(ow-iw)/2:(oh-ih)/2\" -c:v libx264 -b:v 10M -pix_fmt yuv420p -r 30 -shortest -avoid_negative_ts make_zero -fflags +genpts -t 1 " + outputfile
         os.system(cmd)
-        file.write("file \'" + name+'.mp4\'\n')    
+        file.write("file \'" + name +'.mp4\'\n')    
     file.close()
-    cmd = "ffmpeg -f concat -i photo/filenames.txt output.mp4"
-    os.system(cmd)
 
-       
+    cmd = "ffmpeg -f concat -i photo/filenames.txt " + outputname
+    os.system(cmd)       
 
 if __name__ == '__main__':
-    imageurls,filename = get_all_tweets_image("@NintendoAmerica")
-    download_image(imageurls,filename)
-    convert_image_video(filename)
-    vision_analysor(imageurls)
-
+    imageurls,filenames = get_all_tweets_imageurls("@NintendoAmerica",100)
+    download_image(imageurls,filenames)
+    convert_image_video(filenames,'output.mp4')
+    vision_analysor(imageurls,filenames)
+    print("All done!")
 
